ab3f8f7a17b7f95d386beb6e984a6786973174b5
Kyle Nakamura <kjnakamura@apu.edu>
2019-09-18 22:15:11 -0700

    Create template files for problem 1 and 2 of AWS coding interview.

---
+++ Interview Problems/Amazon AWS/problem-2.js
@@ -0,0 +1,41 @@
+/*
+    Amazon Assessment #2
+ */
+function solution() {
+    let arg1 = arguments[0],
+        arg2 = arguments[1];
+
+    let solution = 0;
+
+    return solution
+}
+
+function runTests(func, tests) {
+    let passed = 0;
+    let failed = 0;
+
+    for (let i = 0; i < tests.length; i++) {
+        let test = tests[i];
+        let result = func(...test.input);
+
+        if (result === test.answer) {
+            console.log(`${i + 1}: PASS (${test.input})`);
+            passed += 1;
+        } else {
+            console.log(`${i + 1}: FAIL (${test.input})  -->  (Got: ${result}, Expected: ${test.answer})`);
+            failed += 1;
+        }
+    }
+
+    console.log(`\n${passed} passed, ${failed} failed.`);
+}
+
+runTests(
+    func = solution,
+    tests = [
+        {   // 1
+            input: [1,2,3],
+            answer: 0
+        },
+    ]
+);
